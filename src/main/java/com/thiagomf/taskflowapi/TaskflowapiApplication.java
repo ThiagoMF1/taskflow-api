c24144d32e5855fdab45c4f0ef839af2cd05a1a1
ThiagoMF1 <thigas100@gmail.com>
2026-03-14 11:13:32 -0300

    chore: start taskflow api

---
+++ src/main/java/com/thiagomf/taskflowapi/TaskflowapiApplication.java
@@ -0,0 +1,12 @@
+package com.thiagomf.taskflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskflowapiApplication.class, args);
+    }
+}
