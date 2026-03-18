dbedf8eea865d134fa0213e66d75940b2dcbfda3
ThiagoMF1 <thigas100@gmail.com>
2026-03-18 13:58:15 -0300

    feat: add jwt authentication filter

--- src/main/java/com/thiagomf/taskflowapi/security/JwtService.java
+++ src/main/java/com/thiagomf/taskflowapi/security/JwtService.java
@@ -2,9 +2,10 @@ package com.thiagomf.taskflowapi.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -34,6 +35,15 @@ public class JwtService {
 
     public String extractEmail(String token) {
         return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        String email = extractEmail(token);
+        return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
     }
 
     private Claims extractAllClaims(String token) {
